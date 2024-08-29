2393f6eb786ad5fb0ae9735a7f166d0dce803fd3
Orhan Ozbasaran <73716961+Obihan98@users.noreply.github.com>
2024-08-29 12:06:02 -0400

    Update export-ranger.py

--- export-ranger.py
+++ export-ranger.py
@@ -7,9 +7,6 @@ def execute(ranger_policy_helper):
     
     ranger_config_dict = {
         "output_folder_path": "/tmp",  # Mandatory
-        "service_list": "ranger",  # Optional
-        "exported_policies_file_name": "ranger-policy-output",  # Optional
-        "zone_name": "us-ashburn-1"  # Optional
     }
     
     ranger_policy_helper.export_ranger_policies(ranger_config_dict)
