868565a01a11f9aeafa4d6e7cbddd0f4512ada33
Orhan Ozbasaran <73716961+Obihan98@users.noreply.github.com>
2024-08-29 11:51:51 -0400

    Update export-ranger.py

--- export-ranger.py
+++ export-ranger.py
@@ -6,8 +6,9 @@ def execute(ranger_policy_helper):
     logger.info('Testing Ranger export-import functions')
     
     ranger_config_dict = {
-    "output_folder_path": "/tmp",
-    "output_file_format": "json"
+        "output_folder_path": "/tmp",  # Mandatory param, exports output file to /tmp folder
+        "service_list": "ranger",  # Optional param, exports policies only from hdfs and hive services.
+        "exported_policies_file_name": "ranger-policy-output"
     }
     
     ranger_policy_helper.export_ranger_policies(ranger_config_dict)
